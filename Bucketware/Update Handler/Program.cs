6ee4d271ba85192322d529bc43023b4dc56add50
Emin <97754080+1Emin@users.noreply.github.com>
2022-01-21 12:26:09 +0300

    Add files via upload

---
+++ Bucketware/Update Handler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Update_Handler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
